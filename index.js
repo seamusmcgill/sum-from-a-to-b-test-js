9d4a02ae0f38694a6aa3c5cad5e6899cc2d07d64
Seamus McGill <seamusmcgill@gmail.com>
2022-01-14 17:37:19 -0500

    Created recursive function to sum numbers within a given range

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (toN === fromN) {
+    return fromN;
+  }
+  return toN + sum(fromN, toN - 1);
+
+};
+
+console.log(sum(3, 7));
 
 module.exports = sum;
